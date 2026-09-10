371363a5bb58ede03abde255f0f269cf4c45e865
Wuhealthy <1554531799@qq.com>
2026-09-10 11:24:39 +0800

    重新分配1号和22号道路宝可梦分布

--- src/Tables/wild_encounter_tables.c
+++ src/Tables/wild_encounter_tables.c
@@ -20,76 +20,147 @@ tables to edit:
 
 const struct WildPokemon gRoute1_LandMonsMorning[] =
 {
-	{2, 3, SPECIES_ZIGZAGOON},
-	{2, 3, SPECIES_TAILLOW},
-	{2, 3, SPECIES_LOTAD},
-	{2, 3, SPECIES_SHROOMISH},
-	{2, 3, SPECIES_PAWMI},
-	{2, 3, SPECIES_MINCCINO},
-	{2, 3, SPECIES_MAKUHITA},
-	{2, 3, SPECIES_FLABEBE},
-	{2, 3, SPECIES_WINGULL},
-	{2, 2, SPECIES_VULPIX},
-	{2, 3, SPECIES_ELECTRIKE},
-	{2, 3, SPECIES_RIOLU},
+	{2, 3, SPECIES_ZIGZAGOON},			//蛇纹熊
+	{2, 3, SPECIES_TAILLOW},			//傲骨燕
+	{2, 3, SPECIES_FIDOUGH},			//狗仔包
+	{2, 3, SPECIES_TANDEMAUS},			//一对鼠
+	{2, 3, SPECIES_PAWMI},				//布拨
+	{2, 3, SPECIES_MINCCINO},			//泡沫栗鼠
+	{2, 3, SPECIES_SENTRET},			//尾立
+	{2, 3, SPECIES_FLABEBE},			//花蓓蓓
+	{2, 3, SPECIES_HOOTHOOT},			//咕咕
+	{2, 2, SPECIES_VULPIX},				//六尾
+	{2, 3, SPECIES_ELECTRIKE},			//落雷兽
+	{2, 3, SPECIES_RIOLU},				//利欧路
 };
 
 const struct WildPokemonInfo gRoute1_LandMonsInfoMorning = {15, gRoute1_LandMonsMorning};
 
 const struct WildPokemon gRoute1_LandMonsDay[] =
 {
-    {2, 3, SPECIES_ZIGZAGOON},       // Classic early Normal-type
-    {2, 3, SPECIES_BUIZEL},          // Water-type, Gen 4 freshness
-    {2, 3, SPECIES_ROOKIDEE},        // Flying/Steel later on, Gen 8 bird
-    {2, 3, SPECIES_BUNEARY},         // Fast Normal-type with potential
-    {2,  3, SPECIES_SEWADDLE},        // Bug/Grass option, early status
-    {2,  3, SPECIES_SNOM},            // Unique Ice/Bug type
-    {2,  3, SPECIES_JOLTIK},          // Tiny Electric/Bug variety
-    {2,  3, SPECIES_HOUNDOUR},        // Fire/Dark, adds good offensive typing
-    {1,  3, SPECIES_DEERLING},        // Daytime flavor (Spring form)
-    {2,  3, SPECIES_MUNCHLAX},        // Bulky rare Normal-type
-    {2,  3, SPECIES_DREEPY},          // Ghost/Dragon rare and fun
-    {1,  3, SPECIES_TINKATINK},       // Fairy/Steel rare treasure
+    {2, 3, SPECIES_ZIGZAGOON},			//蛇纹熊
+	{2, 3, SPECIES_TAILLOW},			//傲骨燕
+	{2, 3, SPECIES_FIDOUGH},			//狗仔包
+	{2, 3, SPECIES_TANDEMAUS},			//一对鼠
+	{2, 3, SPECIES_PAWMI},				//布拨
+	{2, 3, SPECIES_MINCCINO},			//泡沫栗鼠
+	{2, 3, SPECIES_SENTRET},			//尾立
+	{2, 3, SPECIES_FLABEBE},			//花蓓蓓
+	{2, 3, SPECIES_HOOTHOOT},			//咕咕
+	{2, 2, SPECIES_VULPIX},				//六尾
+	{2, 3, SPECIES_ELECTRIKE},			//落雷兽
+	{2, 3, SPECIES_MUNCHLAX},			//小卡比兽
 };
 
 const struct WildPokemonInfo gRoute1_LandMonsInfoDay = {15, gRoute1_LandMonsDay};
 
 const struct WildPokemon gRoute1_LandMonsEvening[] =
 {
-    {2, 3, SPECIES_NIDORAN_M},     // Slightly edgy dusk encounter
-    {2, 3, SPECIES_NIDORAN_F},     // Pair with above, classic duo
-    {2, 3, SPECIES_MURKROW},       // Dusk-themed Flying/Dark
-    {2, 3, SPECIES_SNUBBULL},      // Fairy type, temperamental vibes
-    {2, 3, SPECIES_BUNEARY},       // Cute, dusk-active rabbit
-    {2, 3, SPECIES_NICKIT},        // Sly Gen 8 fox, fits dusk perfectly
-    {2, 3, SPECIES_GOSSIFLEUR},    // Soft Grass-type, calm evening wind
-    {2, 3, SPECIES_YAMPER},        // Cheerful Electric-type for contrast
-    {2, 3, SPECIES_DRIFLOON},      // Ghost-type, creepy dusk lore
-    {2, 2, SPECIES_HOUNDOUR},      // Edgy Fire/Dark, good evening fit
-    {2, 3, SPECIES_GLAMEOW},       // Mysterious cat, very dusk-like
-    {2, 3, SPECIES_TOXEL},         // Spiky, dusk-electric baby
+    {2, 3, SPECIES_ZIGZAGOON},			//蛇纹熊
+	{2, 3, SPECIES_MURKROW},			//黑暗鸦
+	{2, 3, SPECIES_DRIFLOON},			//飘飘球
+	{2, 3, SPECIES_TANDEMAUS},			//一对鼠
+	{2, 3, SPECIES_PAWMI},				//布拨
+	{2, 3, SPECIES_MINCCINO},			//泡沫栗鼠
+	{2, 3, SPECIES_SENTRET},			//尾立
+	{2, 3, SPECIES_NOIBAT},				//嗡蝠
+	{2, 3, SPECIES_HOOTHOOT},			//咕咕
+	{2, 2, SPECIES_LITWICK},			//烛光灵
+	{2, 3, SPECIES_BLITZLE},			//斑斑马
+	{2, 3, SPECIES_MUNCHLAX},			//小卡比兽
 };
 
 const struct WildPokemonInfo gRoute1_LandMonsInfoEvening = {15, gRoute1_LandMonsEvening};
 
 const struct WildPokemon gRoute1_LandMonsNight[] =
 {
-    {2, 3, SPECIES_HOOTHOOT},       // Classic owl, perfect for night
-    {2, 3, SPECIES_POOCHYENA},      // Night-prowling Dark-type
-    {2, 3, SPECIES_SPINARAK},       // Nocturnal Bug/Poison
-    {2, 3, SPECIES_CUTIEFLY},       // Gentle fluttery fairy at night
-    {2, 3, SPECIES_SANDSHREW},      // Ground type, fits cooler nights
-    {2, 3, SPECIES_CLEFFA},         // Dreamlike baby Fairy
-    {2, 3, SPECIES_FRIGIBAX},       // Cold-blooded Dragon/Ice baby
-    {2, 3, SPECIES_LITLEO},         // Night hunting Fire-type
-    {2, 3, SPECIES_GASTLY},         // Mandatory spooky ghost 👻
-    {2, 2, SPECIES_MISDREAVUS},     // Pure ghost — mysterious and elegant
-    {2, 3, SPECIES_ZORUA},          // Trickster at night
-    {2, 3, SPECIES_MEOWTH_A},  // Dark-type variant, night stalker
+    {2, 3, SPECIES_GREAVARD},			//墓仔狗
+	{2, 3, SPECIES_MURKROW},			//黑暗鸦
+	{2, 3, SPECIES_DRIFLOON},			//飘飘球
+	{2, 3, SPECIES_MEOWTH},				//喵喵
+	{2, 3, SPECIES_GASTLY},				//鬼斯
+	{2, 3, SPECIES_MINCCINO},			//泡沫栗鼠
+	{2, 3, SPECIES_SENTRET},			//尾立
+	{2, 3, SPECIES_NOIBAT},				//嗡蝠
+	{2, 3, SPECIES_HOOTHOOT},			//咕咕
+	{2, 2, SPECIES_LITWICK},			//烛光灵
+	{2, 3, SPECIES_BLITZLE},			//斑斑马
+	{2, 3, SPECIES_MUNCHLAX},			//小卡比兽
 };
 
 const struct WildPokemonInfo gRoute1_LandMonsInfoNight = {15, gRoute1_LandMonsNight};
 
+const struct WildPokemon gRoute22_LandMonsMorning[] =
+{
+	{2, 3, SPECIES_PIDGEY},          // 波波，经典早期飞行系
+    {2, 3, SPECIES_CATERPIE},        // 绿毛虫，清晨草地常见
+    {2, 3, SPECIES_ODDISH},          // 走路草，清晨草系
+    {2, 3, SPECIES_BELLSPROUT},      // 喇叭芽，草/毒
+    {2, 3, SPECIES_FIDOUGH},         // 狗仔包，清晨活力
+    {2, 3, SPECIES_PAWMI},           // 布拨，电系早起
+    {2, 3, SPECIES_WOOLOO},       	 // 毛辫羊
+    {2, 3, SPECIES_SENTRET},         // 尾立，清晨活跃
+    {2, 3, SPECIES_HOOTHOOT},        // 咕咕，清晨也能遇到
+    {2, 2, SPECIES_VULPIX},          // 六尾，稀有
+    {2, 3, SPECIES_BUDEW},           // 含羞苞，草/毒
+    {2, 3, SPECIES_RIOLU},           // 利欧路，稀有格斗
+};
+
+const struct WildPokemonInfo gRoute22_LandMonsInfoMorning = {15, gRoute22_LandMonsMorning};
+
+const struct WildPokemon gRoute22_LandMonsDay[] =
+{
+    {2, 3, SPECIES_PIDGEY},          // 波波
+    {2, 3, SPECIES_CATERPIE},        // 绿毛虫
+    {2, 3, SPECIES_WEEDLE},          // 独角虫，和绿毛虫对应
+    {2, 3, SPECIES_ODDISH},          // 走路草
+    {2, 3, SPECIES_BELLSPROUT},      // 喇叭芽
+    {2, 3, SPECIES_FIDOUGH},         // 狗仔包
+    {2, 3, SPECIES_PAWMI},           // 布拨
+    {2, 3, SPECIES_WOOLOO},       	 // 毛辫羊
+    {2, 3, SPECIES_MINCCINO},        // 泡沫栗鼠
+    {2, 2, SPECIES_MUNCHLAX},        // 小卡比兽，稀有
+    {2, 3, SPECIES_BOUNSWEET},       // 甜竹竹，草系
+    {2, 3, SPECIES_YAMPER},          // 来电汪，电系
+};
+
+const struct WildPokemonInfo gRoute22_LandMonsInfoDay = {15, gRoute22_LandMonsDay};
+
+const struct WildPokemon gRoute22_LandMonsEvening[] =
+{
+    {2, 3, SPECIES_PIDGEY},          // 波波
+    {2, 3, SPECIES_HOOTHOOT},        // 咕咕，傍晚开始活跃
+    {2, 3, SPECIES_MURKROW},         // 黑暗鸦，黄昏氛围
+    {2, 3, SPECIES_DRIFLOON},        // 飘飘球，傍晚幽灵
+    {2, 3, SPECIES_NOIBAT},          // 嗡蝠，黄昏飞行
+    {2, 3, SPECIES_PAWMI},           // 布拨
+    {2, 3, SPECIES_WOOLOO},       	 // 毛辫羊
+    {2, 3, SPECIES_SENTRET},         // 尾立
+    {2, 3, SPECIES_BLITZLE},         // 斑斑马
+    {2, 2, SPECIES_LITWICK},         // 烛光灵，稀有幽灵
+    {2, 3, SPECIES_GOSSIFLEUR},      // 幼棉棉
+    {2, 3, SPECIES_NICKIT},          // 狡小狐
+};
+
+const struct WildPokemonInfo gRoute22_LandMonsInfoEvening = {15, gRoute22_LandMonsEvening};
+
+const struct WildPokemon gRoute22_LandMonsNight[] =
+{
+    {2, 3, SPECIES_HOOTHOOT},        // 咕咕，夜行经典
+    {2, 3, SPECIES_MURKROW},         // 黑暗鸦
+    {2, 3, SPECIES_DRIFLOON},        // 飘飘球
+    {2, 3, SPECIES_NOIBAT},          // 嗡蝠
+    {2, 3, SPECIES_GASTLY},          // 鬼斯，夜晚幽灵
+    {2, 3, SPECIES_GREAVARD},        // 墓仔狗，夜晚幽灵
+    {2, 3, SPECIES_MEOWTH},          // 喵喵，夜行
+    {2, 3, SPECIES_SENTRET},         // 尾立
+    {2, 3, SPECIES_MINCCINO},        // 泡沫栗鼠
+    {2, 2, SPECIES_LITWICK},         // 烛光灵，稀有
+    {2, 3, SPECIES_ZORUA},           // 索罗亚，恶系
+    {2, 3, SPECIES_MUNCHLAX},        // 小卡比兽，稀有
+};
+
+const struct WildPokemonInfo gRoute22_LandMonsInfoNight = {15, gRoute22_LandMonsNight};
 
 const struct WildPokemonHeader gWildMonMorningHeaders[] =
 {
@@ -97,6 +168,14 @@ const struct WildPokemonHeader gWildMonMorningHeaders[] =
 		.mapGroup = MAP_GROUP(ROUTE_1),
 		.mapNum = MAP_NUM(ROUTE_1),
 		.landMonsInfo = &gRoute1_LandMonsInfoMorning,
+		.waterMonsInfo = NULL,
+		.rockSmashMonsInfo = NULL,
+		.fishingMonsInfo = NULL,
+	},
+	{
+		.mapGroup = MAP_GROUP(ROUTE_22),
+		.mapNum = MAP_NUM(ROUTE_22),
+		.landMonsInfo = &gRoute22_LandMonsInfoMorning,
 		.waterMonsInfo = NULL,
 		.rockSmashMonsInfo = NULL,
 		.fishingMonsInfo = NULL,
@@ -122,6 +201,14 @@ const struct WildPokemonHeader gWildMonDayHeaders[] =
 		.fishingMonsInfo = NULL,
 	},
 	{
+		.mapGroup = MAP_GROUP(ROUTE_22),
+		.mapNum = MAP_NUM(ROUTE_22),
+		.landMonsInfo = &gRoute22_LandMonsInfoDay,
+		.waterMonsInfo = NULL,
+		.rockSmashMonsInfo = NULL,
+		.fishingMonsInfo = NULL,
+	},
+	{
 		.mapGroup = 0xFF,
 		.mapNum = 0xFF,
 		.landMonsInfo = NULL,
@@ -137,6 +224,14 @@ const struct WildPokemonHeader gWildMonEveningHeaders[] =
 		.mapGroup = MAP_GROUP(ROUTE_1),
 		.mapNum = MAP_NUM(ROUTE_1),
 		.landMonsInfo = &gRoute1_LandMonsInfoEvening,
+		.waterMonsInfo = NULL,
+		.rockSmashMonsInfo = NULL,
+		.fishingMonsInfo = NULL,
+	},
+	{
+		.mapGroup = MAP_GROUP(ROUTE_22),
+		.mapNum = MAP_NUM(ROUTE_22),
+		.landMonsInfo = &gRoute22_LandMonsInfoEvening,
 		.waterMonsInfo = NULL,
 		.rockSmashMonsInfo = NULL,
 		.fishingMonsInfo = NULL,
@@ -162,6 +257,14 @@ const struct WildPokemonHeader gWildMonNightHeaders[] =
 		.fishingMonsInfo = NULL,
 	},
 	{
+		.mapGroup = MAP_GROUP(ROUTE_22),
+		.mapNum = MAP_NUM(ROUTE_22),
+		.landMonsInfo = &gRoute22_LandMonsInfoNight,
+		.waterMonsInfo = NULL,
+		.rockSmashMonsInfo = NULL,
+		.fishingMonsInfo = NULL,
+	},
+	{
 		.mapGroup = 0xFF,
 		.mapNum = 0xFF,
 		.landMonsInfo = NULL,
@@ -176,7 +279,11 @@ const struct SwarmData gSwarmTable[] =
 {
 	{
 		.mapName = MAPSEC_ROUTE_1,
-		.species = SPECIES_FRIGIBAX,
+		.species = SPECIES_MUNCHLAX, //小卡比兽
+	},
+	{
+		.mapName = MAPSEC_ROUTE_22,
+		.species = SPECIES_ZORUA, 	//索罗亚
 	},
 };
 
